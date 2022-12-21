d45d89e91c3f1235c587b9c23da4db432f7ad604
Bashir Tarif <112946825+bash199@users.noreply.github.com>
2022-12-22 00:44:56 +0200

    env

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,7 +3,7 @@ import {connect, set} from "mongoose";
 set("strictQuery", false);
 console.log(process.env.CLUSTER_NAME);
 connect(
-   `mongodb+srv://Bash:S0eqlCZJ2k8itSft@cluster0.0nzxfm0.mongodb.net/Musicon`,
+   `mongodb+srv://${process.env.CLUSTER_NAME}:${process.env.CLUSTER_PASS}@cluster0.0nzxfm0.mongodb.net/Musicon`,
    (error, mongoConnection) => {
       if (error) {
          throw new Error("MongoDB connection error: " + error);
