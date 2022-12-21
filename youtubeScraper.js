a9b797ec3de172c0deeab4d4487fdfa90d702f46
omer evron <omerevron011@gmail.com>
2022-12-21 18:33:12 +0200

    refactored for integration

--- youtubeScraper.js
+++ youtubeScraper.js
@@ -1,27 +1,29 @@
 import puppeteer from 'puppeteer';
 
-const searchValue = 'muchachos'
-try {
-    const input = searchValue.split(' ').join('+')
-    console.log(input)
-    const browser = await puppeteer.launch({ headless: true })
-    const page = await browser.newPage()
-    await page.goto(`https://www.youtube.com/results?search_query=${input}`)
-    const results = await page.evaluate(() => {
-        let arr = []
-        const res = document.body.querySelectorAll('#contents > ytd-item-section-renderer > #contents > ytd-video-renderer > #dismissible > div > #meta > #title-wrapper > h3').forEach((e) => {
-            let obj = {
-                title: e.innerText,
-                link: e.querySelector("#video-title").getAttribute('href')
-            }
-            arr.push(obj)
+export const scrape = async (value)=> {
+     const searchValue = value
+    try {
+        const input = searchValue.split(' ').join('+')
+        // console.log(input)
+        const browser = await puppeteer.launch({ headless: true })
+        const page = await browser.newPage()
+        await page.goto(`https://www.youtube.com/results?search_query=${input}`)
+        const results = await page.evaluate(() => {
+            let arr = []
+            const res = document.body.querySelectorAll('#contents > ytd-item-section-renderer > #contents > ytd-video-renderer > #dismissible > div > #meta > #title-wrapper > h3').forEach((e) => {
+                let obj = {
+                    title: e.innerText,
+                    link: `https://www.youtube.com/${e.querySelector("#video-title").getAttribute('href')}` 
+                };
+                arr.push(obj);
+            })
+            return arr
         })
-        return arr
-    })
-
-    console.dir(results)
-
-    browser.close()
-} catch (err) {
-    console.error(err)
+    
+        // console.dir(results)
+        browser.close()
+        return results
+    } catch (err) {
+        console.error(err)
+    }
 }
