77f1c92b06cd782ba19de11be7385681aa47348f
omer evron <omerevron011@gmail.com>
2022-12-21 15:48:33 +0200

    search in youtube

--- youtubeScraper.js
+++ youtubeScraper.js
@@ -7,10 +7,8 @@ try {
     const browser = await puppeteer.launch({headless:false})
     const page = await browser.newPage()
     await page.goto(`https://www.youtube.com/results?search_query=${input}`)
-    const results = await page.evaluate(()=>{
-        const contents = document.querySelector('#contents > ytd-item-section-renderer > #contents')
-        return contents
-    })
+    const results = await page.$$('#contents > ytd-item-section-renderer > #contents')
+
     // const results = await page.waitForSelector('#contents > ytd-item-section-renderer > #contents')
     console.log(results);
     // browser.close()
