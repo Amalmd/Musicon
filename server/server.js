03159e13137b86acd0e4d13bf25fc07d1ae49497
Bashir Tarif <112946825+bash199@users.noreply.github.com>
2022-12-22 00:41:50 +0200

    alaa

--- server/server.js
+++ server/server.js
@@ -8,12 +8,16 @@ import cors from "cors";
 import "./db/mongoose.js";
 import {createUser, login} from "./controllers/controller.js";
 import {scrapeFromYoutube} from "./youtubeScraper.js";
+import * as url from 'url';
+import path from 'path';
+const __dirname = url.fileURLToPath(new URL('./', import.meta.url));
 const PORT = process.env.PORT || 5000;
 const app = express();
 dotenv.config();
 app.use(cors());
 app.use(express.json());
-
+const publicPath = path.join(__dirname,'build');
+app.use(express.static(publicPath));
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
@@ -52,6 +56,11 @@ app.post("/searchVideo", async (req, res) => {
    const getData = await scrapeFromYoutube(req.body.convertedText);
    res.status(200).send(getData);
 });
+console.log(__dirname);
+app.get('*', (req, res) => {
+   console.log(__dirname);
+   res.sendFile(path.resolve(__dirname, 'build','index.html'));
+ });
 
 app.listen(PORT, function () {
    console.log(`server started on port ${PORT} ...`);
