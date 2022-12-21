a27bc9b7d945a9736865454ab5de15a6d0452fb0
ashgolan <ashgolan@hotmail.com>
2022-12-21 18:20:54 +0200

    ..

--- client/src/Components/App.jsx
+++ client/src/Components/App.jsx
@@ -3,14 +3,8 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Error from "./pages/Error";
 import NavBar from "./NavBar";
-import puppeteer from "puppeteer.js";
 
 function App() {
-  (async () => {
-    const browser = await puppeteer.launch({ headless: false });
-    const page = await browser.newPage();
-    await page.goto("https://youtube.com/");
-  })();
   return (
     <>
       <NavBar />
